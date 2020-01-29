dd6537bc00280abb2a77e446d76fa257638ba607
manoelmeseque <meseque.teixeira@outlook.com>
2020-01-29 12:21:21 -0300

    Forma otimizada de ler um arquivo usando o BufferedReader e FileReader

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,14 +9,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		File file = new File("in.txt");
-		FileReader fr = null;
-		BufferedReader fb = null;
-		try {
-			fr = new FileReader(file);
-			fb = new BufferedReader(fr);
+		String file = "in.txt";
+
+		try (BufferedReader fb = new BufferedReader(new FileReader(file))){
 			
 			String line = fb.readLine();
+			
 			while(line != null) {
 				System.out.println(line);
 				line = fb.readLine();
@@ -25,22 +23,7 @@ public class Main {
 		catch(IOException e) {
 			System.out.println("Erro: " + e.getMessage());
 		}
-		finally {
-			
-			try {
-				if(fr != null) {
-					fr.close();
-				}
-				
-				if(fb != null) {
-					fb.close();
-				}
-			}
-			catch(IOException e) {
-				e.printStackTrace();
-			}
-			
-		}
+
 	}
 
 }
